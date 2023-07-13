3a472ce990ad82181f24e8a95b819782927334e5
Katherine Whitlock <kate@skylinesynths.nyc>
2023-07-13 01:03:33 -0400

    Comments and cleaunp

--- src/main.rs
+++ src/main.rs
@@ -50,7 +50,7 @@ fn read_u32(gdb: &mut GdbServer<BufReader<TcpStream>, TcpStream>, addr: u32) -> 
     match gdb.next_packet().unwrap() {
         Some(p) => {
             let data = p.invalidate_check().data;
-            let bytes = hex::decode(data).unwrap();
+            let bytes = hex::decode(data).expect("failed to decode a u32 response");
             let byte_array = [bytes[0], bytes[1], bytes[2], bytes[3]];
             u32::from_le_bytes(byte_array)
         }
@@ -80,8 +80,10 @@ fn cont(gdb: &mut GdbServer<BufReader<TcpStream>, TcpStream>) {
 }
 
 fn main() -> Result<(), core::convert::Infallible> {
-    let display_buf_addr = find_debug_symbol().unwrap();
     const DISPLAY_SIZE: Size = Size::new(128, 48);
+    const DISPLAY_BUF_SIZE: usize = (DISPLAY_SIZE.width * (DISPLAY_SIZE.height >> 3)) as usize;
+
+    let display_buf_addr = find_debug_symbol().unwrap();
     let mut display = SimulatorDisplay::<BinaryColor>::new(DISPLAY_SIZE);
 
     let output_settings = OutputSettingsBuilder::new()
@@ -92,14 +94,13 @@ fn main() -> Result<(), core::convert::Infallible> {
     let mut window =
         embedded_graphics_simulator::Window::new("Deluge OLED output", &output_settings);
 
-    let mut gdb_stream = TcpStream::connect("127.0.0.1:3333").unwrap();
+    // Try to open our stream to GDB and setup our protocol system
+    let mut gdb_stream = TcpStream::connect("127.0.0.1:3333").expect("failed to connect to GDB server");
     let mut gdb = GdbServer::new(BufReader::new(gdb_stream.try_clone().unwrap()), gdb_stream);
 
-    const display_buf_size: usize = (DISPLAY_SIZE.width * (DISPLAY_SIZE.height >> 3)) as usize;
-    let mut displaybuf = [0u8; 1];
-
-    // Desired frequency in Hz
+    // Desired update frequency in Hz
     let frequency: f64 = 24.0;
+
     // Duration between iterations in nanoseconds
     let interval = Duration::from_nanos((1_000_000_000f64 / frequency) as u64);
 
@@ -111,23 +112,31 @@ fn main() -> Result<(), core::convert::Infallible> {
     loop {
         //halt(&mut gdb);
 
-        // Fetch the pointer result of OLED::currentCurrentImage
+        // Fetch the pointer result of OLED::oledCurrentImage
         let current_image_buf_addr = read_u32(&mut gdb, display_buf_addr);
 
-        let request = format!("m{:x},{:x}", current_image_buf_addr, display_buf_size);
+
+        // Read the framebuffer from the deluge by requesting the full block of RAM from GDB
+        let request = format!("m{:x},{:x}", current_image_buf_addr, DISPLAY_BUF_SIZE);
         gdb.dispatch(&CheckedPacket::from_data(Kind::Packet, request.into()))
             .unwrap();
 
+        // Decode the received hex string into a bytes
         let decoded = match gdb.next_packet().unwrap() {
             Some(p) => {
                 let data = p.invalidate_check().data;
-                let bytes = hex::decode(data).unwrap();
+                let bytes = hex::decode(data).expect("failed to decode display buffer read response");
                 bytes.into_iter().map(|b| u8::from_le(b)).collect()
             }
             None => Vec::new(),
         };
 
         //cont(&mut gdb);
+
+        // Image decode ----------------------------------------------------
+
+        // The Deluge (and most SSD1306/SSD1309 display drivers) use a byte-packed column-first page system for storing data
+        // The LSB of byte 0 is (0,0), the MSB of byte 0 is (0,7), byte 1 corresponds to (1, x), etc.
 
         for (page_y, row) in decoded.chunks(DISPLAY_SIZE.width as usize).enumerate() {
             for (x, col) in row.into_iter().enumerate() {
@@ -143,16 +152,25 @@ fn main() -> Result<(), core::convert::Infallible> {
                 }
             }
         }
-        // println!("{:?}", displaybuf);
+        
+        // Display update ---------------------------------------------------------------
+
+        // Generate a "raw" image from our converted display buffer
         let raw_image = ImageRaw::<BinaryColor>::new(&display_buf, DISPLAY_SIZE.width);
         let image = Image::new(&raw_image, Point::zero());
 
+        // Draw the newly generated image on the display
         image.draw(&mut display)?;
 
+        // Update our window with the display
         window.update(&display);
+
+        // Check if a exit has been requested (such as clicking the window exit button)
         if window.events().any(|e| e == SimulatorEvent::Quit) {
             break;
         }
+
+        // Timing tracking --------------------------------------------------------
 
         let elapsed = last_time.elapsed();
 
